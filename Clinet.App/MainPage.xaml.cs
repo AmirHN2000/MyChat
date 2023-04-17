c7066649d4ebefeffe419bfae6b49855f40437ec
Amir <Amir.hnaeemian2000@gmail.com>
2023-04-17 21:46:38 +0430

    add client project and add mudBlazor

---
+++ Clinet.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinet.App;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Clinet.App/MauiProgram.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Logging;
+using Clinet.App.Shared;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using MudBlazor.Services;
+
+namespace Clinet.App;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        var assembly = typeof(MainLayout).GetTypeInfo().Assembly;
+
+        var settings = "appsettings.json";
+#if DEBUG
+        settings = "appsettings.Development.json";
+#endif
+        
+        builder
+            .UseMauiApp<App>()
+            .Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), settings, optional: false, false);
+
+        builder.Services.AddMauiBlazorWebView();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
+
+        builder.Services.AddMudServices();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
